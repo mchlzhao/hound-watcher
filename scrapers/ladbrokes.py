370c7e8f9fa4037d26d870f3616d8bdebb52df68
Michael Zhao <mzhaoau+1@gmail.com>
2022-05-06 22:51:53 +1000

    move processing " nz" to util.process_name

--- scrapers/ladbrokes.py
+++ scrapers/ladbrokes.py
@@ -25,9 +25,6 @@ class LadbrokesScraper(Scraper):
             name = (row_elem
                 .find_element(by=By.XPATH, value='.//span[@class="runner-name"]')
                 .text)
-            # ladbrokes sometimes appends ' Nz' to the name of a runner
-            if name[-3:].lower() == ' nz':
-                name = name[:-3]
 
             fixed_odds_elem = row_elem.find_elements(by=By.XPATH, value='.//td[contains(@class, "runner-fixed-odds")]')
             if len(fixed_odds_elem) == 0:
--- util.py
+++ util.py
@@ -1,4 +1,6 @@
 import re
 
 def process_name(name):
+    if name[-3:].lower() == ' nz':
+        name = name[:-3]
     return re.sub('[^a-zA-z0-9]+', '', name.lower())
